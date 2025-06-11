bcd114c586e0484b6dc407c73a06415218eefa2e
Shubham Gupta <75482143+shubhamdevgupta@users.noreply.github.com>
2025-06-11 15:56:34 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/aesthetic_clinic_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.aesthetic_clinic_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/SplashScreen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/icons/wqmis_splash.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+}
